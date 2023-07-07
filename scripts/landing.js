02953ef90ab533a03bf4522971b4294efff904e1
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-07-07 21:43:57 +0200

    added image, now hover effect

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,28 @@
-export const test = "hello world";
+const paragraphContainer = document.querySelectorAll(
+  ".landing-section-para-container"
+);
 
-export function testTwo() {
-  if (scrollY > 0) {
-    console.log("scrolling");
-  }
-}
+// random color changes on hover for paragraph container
+
+const colorArray = ["#FF6633", "#FFB399", "#FF33FF", "#FFFF99", "#00B3E6"];
+
+const randomColor = () => {
+  return colorArray[Math.floor(Math.random() * colorArray.length)];
+};
+
+const changeColor = (container) => {
+  const color = randomColor();
+  container.style.backgroundColor = color;
+};
+
+paragraphContainer.forEach((container) => {
+  container.addEventListener("mouseenter", () => {
+    changeColor(container);
+  });
+
+  container.addEventListener("mouseleave", () => {
+    container.style.backgroundColor = "transparent";
+  });
+});
+
+// display image on hover for paragraph container
