452073bbb42055b5864f387ef53f0d36fe3123bb
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-07-28 00:56:51 +0200

    started animation, but still stuff to be done

--- scripts/contact/mainButtons.js
+++ scripts/contact/mainButtons.js
@@ -1,11 +1,21 @@
+const contactSectionHide = document.querySelectorAll(
+  ".contact-section :not(.contact-section form *)"
+);
 const buttonForm = document.querySelector(".contact-button");
 const form = document.querySelector(".contact-section form");
 const buttonFormClose = document.querySelector(".form_cross");
 
 buttonForm.addEventListener("click", () => {
   form.classList.add("form-visible");
+  contactSectionHide.forEach((element) => {
+    element.classList.add("contact-section-hidden");
+  });
 });
 
 buttonFormClose.addEventListener("click", () => {
   form.classList.remove("form-visible");
+
+  contactSectionHide.forEach((element) => {
+    element.classList.remove("contact-section-hidden");
+  });
 });
