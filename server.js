3fe686b850e38083e5d506de3bc985933891185f
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-07-28 13:25:51 +0200

    installed dependencies

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+require("dotenv").config({ path: "./keys.env" });
+
 const express = require("express");
-require("dotenv").config();
+const axios = require("axios");
 const app = express();
 const port = 3000;
 const apiKey = process.env.API_KEY;
 
-console.log(apiKey);
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!", apiKey: apiKey });
+
+app.get('/ask', async (req, res) => {
+  const question = req.query.q;  
+
+  const gpt3Response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      prompt: question,
+      max_tokens: 500,  
+  }, {
+      headers: {
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+          'Content-Type': 'application/json',
+      },
+  });
+
+  res.json(gpt3Response.data.choices[0].text.trim());
 });
+
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
