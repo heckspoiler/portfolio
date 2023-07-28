76791de264c77b2b94b1a162b86528152fef6e39
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-07-28 13:34:14 +0200

    test

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./keys.env" });
+require("dotenv").config();
 
 const express = require("express");
 const axios = require("axios");
@@ -6,24 +6,25 @@ const app = express();
 const port = 3000;
 const apiKey = process.env.API_KEY;
 
+app.get("/ask", async (req, res) => {
+  const question = req.query.q;
 
-app.get('/ask', async (req, res) => {
-  const question = req.query.q;  
-
-  const gpt3Response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+  const gpt3Response = await axios.post(
+    "https://api.openai.com/v1/engines/davinci-codex/completions",
+    {
       prompt: question,
-      max_tokens: 500,  
-  }, {
+      max_tokens: 500,
+    },
+    {
       headers: {
-          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-          'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
       },
-  });
+    }
+  );
 
   res.json(gpt3Response.data.choices[0].text.trim());
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
