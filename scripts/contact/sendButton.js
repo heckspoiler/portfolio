be4296c5b86beca43f261a621c80a6f03f98465f
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-07-28 22:24:50 +0200

    chatbot works!!

--- scripts/contact/sendButton.js
+++ scripts/contact/sendButton.js
@@ -10,6 +10,36 @@ chatInput.addEventListener("keydown", function (event) {
   }
 });
 
+const askQuestion = (question) => {
+  fetch(`http://localhost:3000/ask?q=${question}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      displayAnswer(data);
+    })
+    .catch((error) => console.log(error));
+};
+
+function displayAnswer(answer) {
+  let chatAnswer = document.createElement("div");
+  chatAnswer.classList.add("chatbot_message-container");
+  chatAnswer.innerHTML = `<svg width="33" height="42" viewBox="0 0 33 42" fill="none"
+  xmlns="http://www.w3.org/2000/svg">
+  <rect x="1.28894" y="9.91333" width="30.9111" height="30.9111"
+        stroke="#F46265" />
+  <path d="M7.02148 9.41333L7.02148 0.936941" stroke="#F46265" />
+  <line x1="24.2222" y1="9.41333" x2="24.2222" y2="0.936941"
+        stroke="#F46265" />
+  <circle cx="9.76394" cy="20.3828" r="4.98611" fill="#F46265" />
+  <circle cx="23.2263" cy="20.3828" r="4.98611" fill="#F46265" />
+  <rect x="6.27356" y="29.8564" width="20.4431" height="5.48472"
+        fill="#F46265" />
+  <circle cx="8.268" cy="19.8843" r="1.99444" fill="#F9E32C" />
+  <circle cx="24.7222" cy="19.8843" r="1.99444" fill="#F9E32C" />
+  </svg> <p class="chatbot_message">${answer}</p>`;
+  chatContainer.appendChild(chatAnswer);
+}
+
 let firstClick = 0;
 
 sendButton.addEventListener("click", () => {
@@ -18,7 +48,10 @@ sendButton.addEventListener("click", () => {
     textMessage.classList.add("user_message-container");
     textMessage.innerHTML = `<h5>You</h5> <p class="user_message">${chatInput.value}</p>`;
     chatContainer.appendChild(textMessage);
-    chatInput.value = "";
+
+    askQuestion(chatInput.value); // call askQuestion with the current input
+
+    chatInput.value = ""; // then clear the input
     textfieldCount.innerHTML = "0/250";
     firstClick += 1;
   } else {
@@ -35,10 +68,3 @@ sendButton.addEventListener("click", () => {
     chatInput.placeholder = "you seem interested, that's cool!";
   }
 });
-
-{
-  /* <div class="user_message-container">
-<h5>You</h5>
-<p class="user_message"></p>
-</div> */
-}
--- server.js
+++ server.js
@@ -1,30 +1,42 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 const app = express();
 const port = 3000;
 const apiKey = process.env.API_KEY;
 
+app.use(cors());
+
 app.get("/ask", async (req, res) => {
-  const standardPrompt = "I don't know. I'm just a bot.";
+  const standardPrompt = `You’re charlybot, my personal portfolio bot. You’re only allowed to answer questions about me and nothing else. i’ll give you the information needed below and if the person asking you the question asks about something not related to the stuff below, you will tell them that you’re not programmed to answer other questions than questions about me, Carlo. if the person says thank you, goodbye or hello or anything similar, you can answer. but just a short hello or a short goodbye. age: 32My hobbies are: ice hockey, reading, music, programming, fishing.
+  i like: dogs, music, chilling, me-time, family.
+  i dropped out of university because it wasn’t my world and i chose the self taught path because i didn’t want to waste more time and just focus on the stuff that i’m really passionate and interested about. `;
   const question = req.query.q;
 
-  const gpt3Response = await axios.post(
-    "https://api.openai.com/v1/engines/davinci-codex/completions",
-    {
-      prompt: `${standardPrompt}: ${question}`,
-      max_tokens: 500,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        "Content-Type": "application/json",
+  try {
+    const gpt3Response = await axios.post(
+      "https://api.openai.com/v1/engines/text-davinci-003/completions",
+      {
+        prompt: `${standardPrompt}: ${question}`,
+        max_tokens: 500,
       },
-    }
-  );
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  res.json(gpt3Response.data.choices[0].text.trim());
+    console.log(gpt3Response.data);
+
+    res.json(gpt3Response.data.choices[0].text.trim());
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error in processing request");
+  }
 });
 
 app.listen(port, () => {
