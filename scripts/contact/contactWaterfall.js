809b190bbdf4b6b41bdf2b2ba5c50cb09b0f169c
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-08-09 14:10:33 +0200

    animation added to contact section (loading)

---
+++ scripts/contact/contactWaterfall.js
@@ -0,0 +1,34 @@
+const contactsSection = document.querySelector("#contact");
+const title = document.querySelector("#contact h2");
+const items = document.querySelectorAll(
+  ".contact-button, .charlybot-button, #github, #linkedin, #cv"
+);
+
+// observe projectsSection to add animations when visible, loading animations project sections
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        title.classList.add("title-loaded");
+
+        const timeout = 100;
+        setTimeout(() => {
+          items.forEach((item, index) => {
+            setTimeout(() => {
+              item.classList.add("contact-visible");
+            }, timeout * (index + 1));
+          });
+        }, 200);
+      } else {
+        title.classList.remove("title-loaded");
+        items.forEach((item) => {
+          item.classList.remove("list-item-loaded");
+        });
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+observer.observe(contactsSection);
