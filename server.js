e04ce2b0c61956844bd7a8398bcdae6d72a06d9a
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-08-31 17:00:25 +0200

    portfolio finished for now!!

--- server.js
+++ server.js
@@ -79,7 +79,8 @@ app.post("/form", async (req, res) => {
     from: email,
     to: req.body.email,
     subject: `Thank you for reaching out!`,
-    text: `Thank you for your message! I will get back to you as soon as possible. 
+    text: `Hello ${req.body.name}, thanks for reaching out! 
+    I've received your email and will get back to you as soon as possible. Enjoy your day!. 
 Best regards, Carlo
              
 
@@ -89,6 +90,59 @@ Your message:
 Your email:
               ${req.body.email}
              `,
+    html: ` 
+    <html lang="en">
+  <head>
+    <style>
+      h2 {
+        font-size: 1.1rem;
+        font-weight: bold;
+      }
+
+      h3 {
+        font-size: 0.8rem;
+        font-style: italic;
+        font-weight: bold;
+      }
+
+      p {
+        font-size: 0.9rem;
+        font-weight: regular;
+      }
+
+      .text-below { 
+        font-size: 0.7rem;
+      }
+
+      .img {
+        height: 40px;
+        width: 40px;
+        
+      }
+    </style>
+  </head>
+  <body>
+    <h2>Hello ${req.body.name}, thanks for reaching out!</h2>
+    <p>I've received your email and will get back to you as soon as possible. Enjoy your day!</p>
+    <p>Best regards, Carlo</p>
+
+    <br />
+
+    <img src="https://i.ibb.co/yBT69K7/carlo-1.png" alt="carlo-1" border="0">
+    
+
+
+    <br/>
+    <br/>
+    <h3>Your email:</h3>
+    <p class="text-below">${req.body.email}</p>
+
+    <h3>Your message:</h3>
+    <p class="text-below">${req.body.message}</p>
+
+    </body>
+</html>
+    `,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
